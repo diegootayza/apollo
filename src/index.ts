9609c1ff40ac3611db84b45839ba3982aeb408fd
Diego Otayza <groupteknology@outlook.com>
2022-05-28 19:44:46 -0400

    28-05-2022 19:44

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { ApolloServer } from 'apollo-server-express'
-// import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import { createServer } from 'http'
-// import { execute, subscribe } from 'graphql'
-// import { SubscriptionServer } from 'subscriptions-transport-ws'
-// import { useServer } from 'graphql-ws/lib/use/ws'
-// import { WebSocketServer } from 'ws'
+import { useServer } from 'graphql-ws/lib/use/ws'
+import { WebSocketServer } from 'ws'
 import express from 'express'
 
 import schema from './schema'
@@ -14,27 +12,21 @@ const main = async () => {
     await connection()
     const app = express()
     const httpServer = createServer(app)
-    // const wsServer = new WebSocketServer({ path: '/subscriptions', server: httpServer })
-    // const serverCleanup = useServer({ schema }, wsServer)
-
-    // const subscriptionServer = SubscriptionServer.create(
-    //     { schema, execute, subscribe, async onConnect(connectionParams: any, webSocket: any, context: any) {} },
-    //     { path: '/subscriptions', server: httpServer }
-    // )
+    const wsServer = new WebSocketServer({ path: '/subscriptions', server: httpServer })
+    const serverCleanup = useServer({ schema }, wsServer)
 
     const apolloServer = new ApolloServer({
         schema,
         csrfPrevention: false,
         plugins: [
             ApolloServerPluginDrainHttpServer({ httpServer }),
-            // {
-            //     serverWillStart: async () => ({
-            //         drainServer: async () => {
-            // await serverCleanup.dispose()
-            // subscriptionServer.close()
-            //         },
-            //     }),
-            // },
+            {
+                serverWillStart: async () => ({
+                    drainServer: async () => {
+                        await serverCleanup.dispose()
+                    },
+                }),
+            },
         ],
     })
 
