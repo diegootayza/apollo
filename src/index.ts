3d5304d6a9d56e30555aba163e770bbfe0fcf4d3
Diego Otayza <groupteknology@outlook.com>
2022-05-28 15:04:31 -0400

    28-05-2022 15:04

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import { createServer } from 'http'
+import { execute, subscribe } from 'graphql'
+import { SubscriptionServer } from 'subscriptions-transport-ws'
 import { useServer } from 'graphql-ws/lib/use/ws'
 import { WebSocketServer } from 'ws'
 import express from 'express'
@@ -12,8 +14,14 @@ const main = async () => {
     await connection()
     const app = express()
     const httpServer = createServer(app)
-    const wsServer = new WebSocketServer({ path: '/subscriptions', server: httpServer })
-    const serverCleanup = useServer({ schema }, wsServer)
+    // const wsServer = new WebSocketServer({ path: '/subscriptions', server: httpServer })
+    // const serverCleanup = useServer({ schema }, wsServer)
+
+    const subscriptionServer = SubscriptionServer.create(
+        { schema, execute, subscribe, async onConnect(connectionParams: any, webSocket: any, context: any) {} },
+        { path: '/subscriptions', server: httpServer }
+    )
+
     const apolloServer = new ApolloServer({
         schema,
         csrfPrevention: true,
@@ -22,7 +30,8 @@ const main = async () => {
             {
                 serverWillStart: async () => ({
                     drainServer: async () => {
-                        await serverCleanup.dispose()
+                        // await serverCleanup.dispose()
+                        subscriptionServer.close()
                     },
                 }),
             },
